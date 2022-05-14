7e4f8c66533e9d01c770ac5fc3b925b1102eb518
Shoury Sharma <shanuu12e@gmail.com>
2022-05-14 23:17:12 +0530

    Init boilerplate

---
+++ app/src/main/java/com/shanu/stocksapp/StocksApp.kt
@@ -0,0 +1,7 @@
+package com.shanu.stocksapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StocksApp: Application() {}
---
+++ app/src/main/java/com/shanu/stocksapp/data/remote/StockApi.kt
@@ -0,0 +1,18 @@
+package com.shanu.stocksapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apikey: String
+    ): ResponseBody
+
+    companion object {
+        const val API_KEY = "1QK5C2Q2O6O3AW12"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
